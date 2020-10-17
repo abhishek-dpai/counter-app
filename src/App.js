77d2fb43e11d0ba00dc7be911bcbe1b1610cccee
raiab <abhishekr@dailyprophet.ai>
2020-10-17 16:51:07 +0530

    added incr and decr button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return "Hello Ab!!";
+  const [cnt, setCnt] = useState(0);
+  function handleDecr() {
+    setCnt(cnt - 1);
+  }
+  function handleIncr() {
+    setCnt(cnt + 1);
+  }
+  return (
+    <>
+      <button onClick={handleDecr}>-</button>
+      <h2>{cnt}</h2>
+      <button onClick={handleIncr}>+</button>
+    </>
+  );
 }
 
 export default App;
